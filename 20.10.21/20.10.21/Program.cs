5e4e34cc841063b7f53a03d3eeca3281138efd43
Murad R <90379851+sobaking3@users.noreply.github.com>
2021-10-20 13:52:46 +0300

    Add files via upload

---
+++ 20.10.21/20.10.21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _20._10._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint i, j;
+            uint[,] A = new uint[5, 5]{ { 25 , 55 , 15 , 75 , 255 } ,
+     { 7525 , 1715 , 2525 , 71515 , 555 } ,
+     { 20 , 50 , 700 , 800 , 1000 } ,
+     { 22 , 72 , 92 , 32 , 102 } ,
+     { 73  , 883 , 353 , 703 , 2283 }
+   };
+            for (i = 0; i <= (5 - 1); i++)
+            {
+                uint sum = 0;
+                for (j = 0; j <= (5 - 1); j++)
+                {
+                    sum += A[i, j];
+                }
+                Console.WriteLine(sum+ "\n\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
